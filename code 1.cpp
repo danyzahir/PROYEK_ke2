cd99ecc9a2f46f6bb61f09e5728982213712c0f3
unknown <zhelfyputrirapunzellia@gmail.com>
2023-12-11 14:00:01 +0700

    tambahan2

--- code 1.cpp
+++ code 1.cpp
@@ -88,3 +88,60 @@ void inputData() {
     // Menambahkan pembatas
     cout << "\n----------------------------------------\n";
 }
+void updateData() {
+    string searchName;
+    cout << "Masukkan nama untuk update data: ";
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    getline(cin, searchName);
+
+    auto it = find_if(database.begin(), database.end(), [searchName](const Data &d) { return d.nama == searchName; });
+
+    if (it != database.end()) {
+        cout << "Update data untuk " << searchName << ":\n";
+        // Implement update logic here
+        cout << "Nama: ";
+        getline(cin, it->nama);
+
+        cout << "Tempat Tanggal Lahir: ";
+        getline(cin, it->tempatTanggalLahir);
+
+        cout << "Usia: ";
+        cin >> it->usia;
+
+        cin.ignore();
+        cout << "Jenis Kelamin: ";
+        getline(cin, it->jenisKelamin);
+
+        cout << "Agama: ";
+        getline(cin, it->agama);
+
+        cout << "Status Perkawinan: ";
+        getline(cin, it->statusPerkawinan);
+
+        cout << "Riwayat Pekerjaan: ";
+        getline(cin, it->riwayatPekerjaan);
+
+        cout << "Partai: ";
+        getline(cin, it->partai);
+
+        cout << "Data berhasil diupdate.\n";
+    } else {
+        cout << "Data tidak ditemukan.\n";
+    }
+}
+
+void removeData() {
+    string searchName;
+    cout << "Masukkan nama untuk menghapus data: ";
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    getline(cin, searchName);
+
+    auto it = remove_if(database.begin(), database.end(), [searchName](const Data &d) { return d.nama == searchName; });
+
+    if (it != database.end()) {
+        database.erase(it, database.end());
+        cout << "Data untuk " << searchName << " berhasil dihapus.\n";
+    } else {
+        cout << "Data tidak ditemukan.\n";
+    }
+}
