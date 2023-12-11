4887cb833eda2b9121f87a78e95874749c8ec2c9
unknown <whydn2308@gmail.com>
2023-12-11 12:15:08 +0700

    Tambahan

--- code 1.cpp
+++ code 1.cpp
@@ -52,3 +52,39 @@ void inputData() {
     // Menambahkan pembatas
     cout << "\n----------------------------------------\n";
 }
+vector<Data> database;
+
+void inputData() {
+    Data newData;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    cout << "Nama: ";
+    getline(cin, newData.nama);
+
+    cout << "Tempat Tanggal Lahir: ";
+    getline(cin, newData.tempatTanggalLahir);
+
+    cout << "Usia: ";
+    cin >> newData.usia;
+
+    cin.ignore();
+    cout << "Jenis Kelamin: ";
+    getline(cin, newData.jenisKelamin);
+
+    cout << "Agama; ";
+    getline(cin, newData.agama);
+
+    cout << "Status Perkawinan: ";
+    getline(cin, newData.statusPerkawinan);
+
+    cout << "Riwayat Pekerjaan: ";
+    getline(cin, newData.riwayatPekerjaan);
+
+    cout << "Partai: ";
+    getline(cin, newData.partai);
+
+    database.push_back(newData);
+
+    // Menambahkan pembatas
+    cout << "\n----------------------------------------\n";
+}
